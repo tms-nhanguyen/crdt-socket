85a0181e573f560896b7cd64189b4f87a3215551
nha <nha.nguyen@tomosia.com>
2025-09-25 08:41:30 +0700

    refactor

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ import { setupWSConnection } from '@y/websocket-server/utils'
 
 const wss = new WebSocketServer({ noServer: true })
 
-const host = process.env.HOST || 'localhost'
 const port = process.env.PORT || 1234
 
 const server = http.createServer((request, response) => {
@@ -31,6 +30,6 @@ server.on('upgrade', (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, handleAuth)
 })
 
-server.listen(port, host, () => {
-  console.log(`running at '${host}' on port ${port}`)
+server.listen(port, () => {
+  console.log(`running on port ${port}`)
 })
