71df7940718bb567400c739c774909e9a10896ea
nha <nha.nguyen@tomosia.com>
2025-09-25 08:26:52 +0700

    refactor

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+#!/usr/bin/env node
 
-import express from 'express'
-import expressWs from 'express-ws'
-// @ts-expect-error import directly from dist folder
+/**
+ * @type {any}
+ */
+import { WebSocketServer } from "ws";
+import http from 'http'
 import { setupWSConnection } from '@y/websocket-server/utils'
 
-const { app } = expressWs(express())
-const port = process.env.PORT || 3333
+const wss = new WebSocketServer({ noServer: true })
 
-app.use(express.json())
+const host = process.env.HOST || 'localhost'
+const port = process.env.PORT || 1234
 
-app.ws('/', (ws, req) => {
-    setupWSConnection(ws, req, { docName: 'public' })
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end('okay')
 })
 
-app.listen(port, () => {
-    console.log(`express server started on ${port}`)
+wss.on('connection', setupWSConnection)
+
+server.on('upgrade', (request, socket, head) => {
+  // You may check auth of request here..
+  // See https://github.com/websockets/ws#client-authentication
+  /**
+   * @param {any} ws
+   */
+  const handleAuth = ws => {
+    wss.emit('connection', ws, request)
+  }
+  wss.handleUpgrade(request, socket, head, handleAuth)
+})
+
+server.listen(port, host, () => {
+  console.log(`running at '${host}' on port ${port}`)
 })
